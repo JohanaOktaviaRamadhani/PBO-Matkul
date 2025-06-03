0a85d6f73ce8761f96fa415a974b22f7220fcec2
JohanaOktaviaRamadhani <hanaoktavia82281@gmail.com>
2025-06-03 14:41:35 +0700

    Minggu ke 10 buat abstract

---
+++ week10/Mobil.java
@@ -0,0 +1,13 @@
+package week10;
+
+public class Mobil extends Kendaraan {
+	private int kapasitasBagasi;
+	
+	@Override
+	public void nyalakan() {
+		// TODO Auto-generated method stub
+		System.out.println("Mobil dinyalakan");
+		
+	}
+
+}
